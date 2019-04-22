bdc05eb557a19fcbd282ef723f36d90be4f9874c
columbus139 <46508712+columbus139@users.noreply.github.com>
2019-04-22 18:50:37 -0400

    Add files via upload

---
+++ trinity-desktop.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo rpm -Uvh http://mirror.ppa.trinitydesktop.org/trinity/trinity/rpm/f29/trinity-r14/RPMS/noarch/trinity-repo-14.0.6-1.fc29.noarch.rpm
+#sudo dnf install trinity-tdebase #Minimal
+#sudo dnf install trinity-desktop #Full w/o Apps
+#sudo dnf install trinity-desktop-all #Full w/ Apps
